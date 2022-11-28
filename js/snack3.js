5428fb6774318bc2426d1e35e3acda964e1f9810
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-28 17:20:10 +0100

    add corrections

--- js/snack3.js
+++ js/snack3.js
@@ -9,6 +9,6 @@ const nums = [2,321,23,243,4,45,5,5,43,34,4,3,43];
 const numsMin = [2,321,23,243,4,4];
 
 while(numsMin.length !== nums.length){
-    numsMin.push(Math.floor(Math.random * 1000));
+    numsMin.push(Math.floor(Math.random() * 1000));
 }
 console.log(nums, numsMin);
