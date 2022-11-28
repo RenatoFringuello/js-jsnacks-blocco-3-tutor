2e7f9cf1df4861a4f18d6eb6b6a347156e9b0628
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-28 16:39:58 +0100

    add snack2

--- js/snack2.js
+++ js/snack2.js
@@ -1,2 +1,16 @@
+// Crea un array vuoto e chiedi all'utente un numero da 
+//inserire nell'array. Continua a chiedere i numeri all'utente
+// e a inserirli nell'array fino a
+//quando la somma degli elementi è minore di 50.
+
 //snack tutors
 console.log('snack 2\n-------------------------------------');
+const nums = [];
+let sum = 0;
+do {
+    let n = parseInt(prompt('inserisci un numero'));
+    if(!Number.isNaN(n)){
+        nums.push(n);
+        sum += n;
+    }
+} while (sum < 50);
