5a3299cbb80ac50631b0b9e40e1910df7af50388
RenatoFringuello <renatofringuello@hotmail.it>
2022-11-28 16:20:13 +0100

    remove ex rick from snack1

--- js/snack1.js
+++ js/snack1.js
@@ -1,23 +1,3 @@
-//esercizio rick w/ while instead of for loop
-const ulElement = document.getElementById('list');
-const list = [
-    'camion',
-    'gilet',
-    'bike',
-    'car',
-    'bread',
-    'milk',
-    'shoes'
-];
-
-while(ulElement.children.length < list.length){
-    //add li item
-    const liElement = document.createElement('li');
-    liElement.classList.add('list-group-item');
-    liElement.append(list[ulElement.children.length]);
-    ulElement.append(liElement);
-}
-
 //snack tutors
 console.log('snack 1\n-------------------------------------');
 
